d948de2efeab487c7fab43a62008c106f29a0d3b
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-13 18:03:00 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        title: "MyFlix [Angular]",
+        subtitle: "MEAN Stack",
+        description: "MyFlix was built using Angular, TypeScript and Angular Materials.",
+        image: "img/Profile-view.png",
+        linkdemo: "https://bethanykeplinger.github.io/myFlix-Angular-client/welcome",
+        linkcode: "https://github.com/BethanyKeplinger/myFlix-Angular-client",
+    },
+]
