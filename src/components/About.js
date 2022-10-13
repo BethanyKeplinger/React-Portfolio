4d5aa5cd79068c603ab4df2a1ac3db71b9bdfd7f
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-13 18:02:25 -0400

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section id="about">
+            <div className="aboutSection">
+                 <p>Hello! I’m Bethany. I’m a web developer living in Portland, Maine seeking to support environmental
+                    sustainability in my work life. After several years in childhood
+                    development, I enrolled in a coding bootcamp and it turned out that programming required many similar
+                    skills, like problem
+                    solving and multi-tasking. As a developer, I want to use code to solve some of the planets’
+                    biggest problems.</p>
+
+                <p>When I'm not working on my web development skills, you can find me in my garden, at a greenhouse
+                   or thrifting to find new treasures for my home.</p>
+
+                <p>I am currently available for full-time work in the New England area or remotely.</p>
+
+                {/* <p class="resume-link"> Download my Resume! <a href="files/Keplinger-Bethany-Resume.pdf" target="_blank"
+                   title="Download Resume"><img src="img/download.svg" class="download-image" alt="download-resume-logo"></a></p> */}
+            </div>
+        </section>
+    )
+
+}
